dedfd862a44d4cf62c9b98d2e36f5b131bb09579
Jeewon Lee <jeewon.jlee@gmail.com>
2021-11-28 22:22:25 +0000

    Remove initial code for index.ejs and add comments for create-password-router.ejs

--- routes/create-password-router.js
+++ routes/create-password-router.js
@@ -1,9 +1,13 @@
 const express = require("express");
 const router = express.Router();
 
-// GET /passwords/create
+// GET requests
+
+// Renders Create New Password page
 router.get("/", (req, res) => {
   res.render("password_create");
 });
+
+// POST requests
 
 module.exports = router;
