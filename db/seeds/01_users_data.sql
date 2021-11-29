6913d0014486a665b2b357f3ae04de264e3f440f
Mei Nghiem-Lin <mcnghiem@gmail.com>
2021-11-29 06:36:15 +0000

    add data to users

--- db/seeds/01_users_data.sql
+++ db/seeds/01_users_data.sql
@@ -1,3 +1,20 @@
--- Users table seeds here (Example)
-INSERT INTO users (name, password, email, organization_id) VALUES ('Alice','password','alice@example.com',1);
+INSERT INTO users (name, email, password, organization_id, admin) VALUES
+('Jake Peralta','coolcoolcool@gmail.com','password',1, false),
+('Andre Holt','admin@NYPD99.com','password',1, true),
+('Amy Santiago','hallmonitor@yahoo.com','password',2, false),
+('Rosa Diaz','ro-ro@example.com','password',4, false),
+('Terry Jeffords','sargeterbear@NYPD99.com','password',2, true)
+('Sadie Sanford', 'gutmann_deondre@yahoo.com', 4, false );
+('Dax Schimmel', 'johnson_lenora@yahoo.com', 1, false );
+('Janie Kihn', 'desmond_wehner@yahoo.com', 4, false );
+('Giovanny Bradtke', 'leatha.lindgren@hotmail.com', 1, false );
+('Kassandra Beahan', 'carter_jaskolski@yahoo.com', 8, false);
+('Emmy Effertz', 'damien_bruen@yahoo.com', 7, )false;
+('Baylee Lueilwitz', 'pinkie.rogahn@yahoo.com', 7, false);
+('Ellis Hickle', 'legros.judy@corrine.us', 1, true);
+('Wanda Mante', 'felton.muller@grant.biz', 9, false);
+('Okey Jaskolski', 'mcdermott.jack@yahoo.com', 2, false );
+('Gene Carter', 'javonte_sauer@gregg.com', 3, false );
+('Mabel Wunsch', 'marina.windler@rachael.io', 1, false);
+
 
