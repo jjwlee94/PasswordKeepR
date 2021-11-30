471cd0b5706c11a65285ea2cad716971dab6bf31
Jeewon Lee <jeewon.jlee@gmail.com>
2021-11-30 05:50:43 +0000

    Modify styling of create password page and add new password_all.ejs file for all passwords

--- routes/index.js
+++ routes/index.js
@@ -22,7 +22,7 @@ router.get("/", (req, res) => {
 });
 // Renders passwords page
 router.get("/passwords", (req, res) => {
-  res.render("index");
+  res.render("password_all");
 });
 
 module.exports = router;
