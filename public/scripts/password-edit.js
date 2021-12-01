99e4981b3fd3dd618478c538ffb3f77b85ad8ba9
Jeewon Lee <jeewon.jlee@gmail.com>
2021-12-01 17:42:47 +0000

    Add function edit and save buttons

--- public/scripts/password-edit.js
+++ public/scripts/password-edit.js
@@ -1,13 +1,15 @@
-const paragraph = document.getElementById("myInput");
-const edit_button = document.getElementById("edit-button");
-const end_button = document.getElementById("save-button");
+$(document).ready(function () {
+  const paragraph = document.getElementById("password");
+  const edit_button = document.getElementById("edit-button");
+  const save_button = document.getElementById("save-button");
 
-edit_button.addEventListener("click", function () {
-  paragraph.contentEditable = true;
-  paragraph.style.backgroundColor = "#dddbdb";
-});
+  edit_button.addEventListener("click", function () {
+    paragraph.contentEditable = true;
+    paragraph.style.backgroundColor = "#dddbdb";
+  });
 
-end_button.addEventListener("click", function () {
-  paragraph.contentEditable = false;
-  paragraph.style.backgroundColor = "#ffe44d";
+  save_button.addEventListener("click", function () {
+    paragraph.contentEditable = false;
+    paragraph.style.backgroundColor = "white";
+  });
 });
