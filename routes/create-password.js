b1611bd4bb39550a7360faaa9348a692eca604e8
Jeewon Lee <jeewon.jlee@gmail.com>
2021-12-01 21:58:00 +0000

    Add possible query for createPassword function

--- routes/create-password.js
+++ routes/create-password.js
@@ -16,6 +16,30 @@ app.use(
 
 // GET requests
 
+// const createPassword = function (db) {
+//   queryString = `
+//   INSERT INTO passwords (user_id, website_url, website_username, website_password, category_id, organization_id)
+//   VALUES ($1, $2, $3, $4, $5, $6)
+//   RETURNING *;
+//   `;
+//   queryParams = [
+//     passwords.user_id,
+//     passwords.website_url,
+//     passwords.website_username,
+//     passwords.website_password,
+//     passwords.category_id,
+//     passwords.organization_id,
+//   ];
+//   return db
+//     .query(queryString, queryParams)
+//     .then((res) => {
+//       return res.rows[0];
+//     })
+//     .catch((err) => {
+//       return console.log(err);
+//     });
+// };
+
 // Renders Create New Password page
 router.get("/", (req, res) => {
   res.render("password_create");
