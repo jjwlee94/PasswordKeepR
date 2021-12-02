609c41f447d09b88211f6a599061ab4b2f191f2e
Jeewon Lee <jeewon.jlee@gmail.com>
2021-12-02 19:51:58 +0000

    Adjust passwords seeds

--- db/seeds/04_passwords_data.sql
+++ db/seeds/04_passwords_data.sql
@@ -1,19 +1,12 @@
 INSERT INTO passwords (website_url, website_username, website_password, user_id, category_id, organization_id) VALUES
-('https://www.youtube.com/', 'github@youtube.com','sAqW(9B@jO',1,3,1 ),
-('https://www.facebook.com/', 'github@facebook.com','B%wD>~WPt',1,1,1 ),
-('https://www.linkedin.com/', 'github@linkedin.com','6~:u}EfEIT{+(FUe',1,2,1 ),
-('https://www.twitter.com/', 'github@twitter.com','ZC}DL6KWD8ztk-@E#,1i',1,3,1 ),
-('https://www.reddit.com/', 'github@reddit.com','HznTqwGLAuLENaJ',1,3,1 ),
-('https://www.tdcanadatrust.com/', 'github@td.com','pre7gbivrs9xenh',1,2,1 ),
-('https://www.staples.ca/', 'github@staples.ca','86lM9kk9',1,2,1),
-('https://wealthsimple.com/', 'github@wealthsimple.com','<CtN$!2M',1,3,1),
+('www.youtube.com', 'github@youtube.com','sAqW(9B@jO',1,3,1 ),
+('www.facebook.com', 'github@facebook.com','B%wD>~WPt',1,1,1 ),
+('www.linkedin.com', 'github@linkedin.com','6~:u}EfEIT{+(FUe',1,2,1 ),
+('www.twitter.com', 'github@twitter.com','ZC}DL6KWD8ztk-@E#,1i',1,3,1 ),
+('www.reddit.com', 'github@reddit.com','HznTqwGLAuLENaJ',1,3,1 ),
+('www.tdcanadatrust.com', 'github@td.com','pre7gbivrs9xenh',1,2,1 ),
+('www.staples.ca', 'github@staples.ca','86lM9kk9',1,2,1),
+('www.wealthsimple.com', 'github@wealthsimple.com','<CtN$!2M',1,3,1),
 
 
-('https://www.youtube.com/', 'github@youtube.com','sAqW(9B@jO',2,3,1 ),
-('https://www.facebook.com/', 'github@facebook.com','B%wD>~WPt',2,1,1 ),
-('https://www.linkedin.com/', 'github@linkedin.com','6~:u}EfEIT{+(FUe',2,2,1 ),
-('https://www.twitter.com/', 'github@twitter.com','ZC}DL6KWD8ztk-@E#,1i',2,3,1 ),
-('https://www.reddit.com/', 'github@reddit.com','HznTqwGLAuLENaJ',2,3,1 ),
-('https://www.tdcanadatrust.com/', 'github@td.com','pre7gbivrs9xenh',2,2,1 ),
-('https://www.staples.ca/', 'github@staples.ca','86lM9kk9',2,2,1),
-('https://wealthsimple.com/', 'github@wealthsimple.com','<CtN$!2M',2,3,1);
+('www.lighthouselabs.ca', 'github@lighthouselabs.com','sAqW(9B@jO',2,3,1 );
